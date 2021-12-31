638aa4c39281525bc3b9099e82f29ae450404cad
ardomingo <andres.domingo@outlook.com>
2021-12-31 20:53:20 +0000

    EDA Script Init

---
+++ EDA.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(COVID19)
+library(usethis)
+
+
+usethis::use_git_config(user.name = "ardomingo", user.email = "andres.domingo@outlook.com")
+token <- "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"
+
+credentials::set_github_pat("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")
